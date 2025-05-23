cd68ce02831735c3cc2673a770fc4b2b9cbe9f2c
Busturdelahess <jules.ledoux0322@outlook.fr>
2025-05-23 16:54:26 +0200

    v3 : réactualisation de back end

--- backend.py
+++ backend.py
@@ -13,10 +13,10 @@ def encode_image(image_path):
         with open(image_path, "rb") as image_file:
             return base64.b64encode(image_file.read()).decode("utf-8")
     except FileNotFoundError:
-        print(f"Error: The file {image_path} was not found.")
+        (f"Error: The file {image_path} was not found.")
         return None
     except Exception as e:
-        print(f"Error: {e}")
+        (f"Error: {e}")
         return None
 
 
